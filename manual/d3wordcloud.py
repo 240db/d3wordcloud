fd99c0930809120a91dd882e813cdb75b7bc7577
240db <nicktup@240db.com>
2024-04-15 00:13:17 -0300

    Update d3wordcloud.py

--- manual/d3wordcloud.py
+++ manual/d3wordcloud.py
@@ -3,7 +3,7 @@ from IPython.display import display, HTML
 def save_wordcloud(text):
     with open('d3wordcloud-template.html') as file:
         wc = file.read().replace("$text",text)
-    with open('./saving.html', 'rw+') as f:
+    with open('./saving.html', 'w+') as f:
         f.write(wc)
         f.close
 
