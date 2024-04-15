a59f11e4ba1eea8ebb9c4a5b850f16a466ca0781
240db <nicktup@240db.com>
2024-04-15 02:24:28 -0300

    Update d3wordcloud.py

--- manual/d3wordcloud.py
+++ manual/d3wordcloud.py
@@ -1,12 +1,13 @@
 from IPython.display import display, HTML
 
-def save_wordcloud(text):
+def save_wordcloud(text, show_settings=False):
     with open('d3wordcloud-template.html') as file:
         wc = file.read().replace("$text",text)
+         if show_settings == False: # hidden=true doesn't work for some reason
+            wc = wc.replace("""<form id="form" style="">""","""<form id="form" style="display:none">""")  
     with open('./saving.html', 'w+') as f:
         f.write(wc)
         f.close
-
 
 def display_wordcloud(text, show_settings=False):
     with open('d3wordcloud-template.html') as file:
